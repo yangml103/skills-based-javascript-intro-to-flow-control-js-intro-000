2db9f5f7a39fc57080dfad4909787351a7ac5a8e
Anthony Yang <anthony.yang103@gmail.com>
2020-05-17 21:22:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,8 @@
 function basicTeenager(age) {
   if (age <= 19 && >= 13 ) {
-    console.log("You are a teenager!")
+    return("You are a teenager!")
   } else {
-    console.log("You are not a teenager!")
+    return("You are not a teenager!")
   }
 }
 
